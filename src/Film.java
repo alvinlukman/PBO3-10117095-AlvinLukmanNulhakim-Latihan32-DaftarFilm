2a50a344ba8fa88220d08803f093405565cd578a
alvinlukmanNH <galinx20gmail.com>
2018-10-08 22:56:25 +0700

    Film

---
+++ src/Film.java
@@ -0,0 +1,29 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/**
+ *
+ * @author 
+ * Nama             : Alvin Lukman Nulhakim
+ * Nim              : 10117095
+ * Kelas            : IF-3
+ * Deskripsi        : Program Daftar Film
+ */
+class Film {
+
+    public String filmName;
+    public String filmGenre;
+    public double filmRating;
+    public int filmDuration;
+
+    public void nowPlaying(String parJudul, String parGenre, double parRating, int parDuration){
+        System.out.println("Judul Film  : "+parJudul);
+        System.out.println("Genre Film  : "+parGenre);
+        System.out.println("Rating Film : "+parRating);
+        System.out.println("Duration Film : "+parDuration+" menit\n");
+    
+    }
+}
